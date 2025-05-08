224b0875678ea1623654d94ec891bc5f5ecb7c5a
0in123 <mdunbarhere@gmail.com>
2025-05-08 16:24:22 +0100

    git commit -m 'message'

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/assignment1.js
@@ -0,0 +1,5 @@
+var Sent1 = "This is the beginning of the string"; // I am assigning a string to variable Sent1
+var Sent2 = " and this is the end of the string"; // I am assigning a string to variable Sent2
+
+window.alert("Hello! Did you know that 5+3 = " + (5 + 3) + "?"); // I am showing a pop up alert with the value of 5+3 and a string concatenated to it to show the answer
+
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "I am assigning a string to variable A"; // I am assigning a string to variable A
+window.alert(A); //I am showing a pop up alert with the value of A (the string)
+
+document.write("Hello, World!") // I am writing "Hello, World!" so that it appears on the page
+
+var B = ("\"I finished watching Yellowstone last night!" // I am using concatenation to add a string to the variable B
+    +" I absolutely loved it!\"");
+
+var Family = "The Dunbars", Son = "Michael", Mum = "Alison", Dad = "Bob", Dog = "Toby"; // I am declaring multiple variables in one line
+
+5+4 // This is an expression that evaluates to 9
